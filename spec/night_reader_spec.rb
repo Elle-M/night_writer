51297a7c4dd2f6561e2f4b7690cd153f29f9bd1c
Elle Majors <hellolovely@ellemajors.com>
2023-01-15 16:15:29 -0700

    test: #braile_to_letter can access inverted alphabet hash

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe NightReader do
   it 'exists' do
     expect(night_reader).to be_a(NightReader)
   end
+
+  xit 'has attributes' do
+    #combine with attr_reader
+    # allow(night_writer).to recieve(:read_file).and_return('./message.txt')
+    # allow(night_writer).to recieve(:read_file).and_return('./braille.txt')
+    night_reader.read_and_write
+
+    expect(night_reader.read_file).to eq('./braille.txt')
+    expect(night_reader.write_file).to eq('./message.txt')
+  end
+
+  it 'has a hash that has braille keys and letter values' do
+    expect(night_reader.braille_to_letter.first).to eq([["0.", "..", ".."], "a"])
+  end 
 end
