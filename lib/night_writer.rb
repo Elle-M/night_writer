f618c93626eaa364bbb2a9e2282d6305ccfe8c7b
Elle Majors <hellolovely@ellemajors.com>
2023-01-15 14:57:28 -0700

    refact: to include Alphabetable module

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,39 +1,40 @@
+require './alphabetable'
 class NightWriter
   attr_accessor :read_file,
                 :write_file
-  attr_reader :alphabet
-
+  # attr_reader :alphabet
+  include Alphabetable
   def initialize
     @read_file = ARGV[0]
     @write_file = ARGV[1]
-    @alphabet = {
-      "a" => ["0.", "..", ".."],
-      "b" => ["00", "..", ".."],
-      "c" => ["0.", ".0", ".."],
-      "d" => ["0.", ".0", "0."],
-      "e" => ["0.", "..", "0."],
-      "f" => ["00", ".0", ".."],
-      "g" => ["00", ".0", "0."],
-      "h" => ["00", "..", "0."],
-      "i" => [".0", ".0", ".."],
-      "j" => [".0", ".0", "0."],
-      "k" => ["0.", "0.", ".."],
-      "l" => ["00", "0.", ".."],
-      "m" => ["0.", "00", ".."],
-      "n" => ["0.", "00", "0."],
-      "o" => ["0.", "0.", "0."],
-      "p" => ["00", "00", ".."],
-      "q" => ["00", "00", "0."],
-      "r" => ["00", "0.", "0."],
-      "s" => [".0", "00", ".."],
-      "t" => [".0", "00", "0."],
-      "u" => ["0.", "0.", ".0"],
-      "v" => ["00", "0.", ".0"],
-      "w" => [".0", ".0", "00"],
-      "x" => ["0.", "00", ".0"],
-      "y" => ["0.", "00", "00"],
-      "z" => ["0.", "0.", "00"]
-      }
+    # @alphabet = {
+    #   "a" => ["0.", "..", ".."],
+    #   "b" => ["00", "..", ".."],
+    #   "c" => ["0.", ".0", ".."],
+    #   "d" => ["0.", ".0", "0."],
+    #   "e" => ["0.", "..", "0."],
+    #   "f" => ["00", ".0", ".."],
+    #   "g" => ["00", ".0", "0."],
+    #   "h" => ["00", "..", "0."],
+    #   "i" => [".0", ".0", ".."],
+    #   "j" => [".0", ".0", "0."],
+    #   "k" => ["0.", "0.", ".."],
+    #   "l" => ["00", "0.", ".."],
+    #   "m" => ["0.", "00", ".."],
+    #   "n" => ["0.", "00", "0."],
+    #   "o" => ["0.", "0.", "0."],
+    #   "p" => ["00", "00", ".."],
+    #   "q" => ["00", "00", "0."],
+    #   "r" => ["00", "0.", "0."],
+    #   "s" => [".0", "00", ".."],
+    #   "t" => [".0", "00", "0."],
+    #   "u" => ["0.", "0.", ".0"],
+    #   "v" => ["00", "0.", ".0"],
+    #   "w" => [".0", ".0", "00"],
+    #   "x" => ["0.", "00", ".0"],
+    #   "y" => ["0.", "00", "00"],
+    #   "z" => ["0.", "0.", "00"]
+    #   }
   end
 
   def read_and_write
@@ -53,7 +54,7 @@ class NightWriter
   def text_to_braile(message)
     # message = File.open(@read_file)
     braille_message = message.chars.filter_map do |letter|
-      @alphabet[letter]
+      letter_to_braille[letter]
     end
     sliced_array = braille_message.transpose.map do |braille|
       # braille.join.chars.each_slice(80).map do |slice|
@@ -74,6 +75,6 @@ class NightWriter
 end
 
 
-  night_writer = NightWriter.new
-  night_writer.read_and_write
+  # night_writer = NightWriter.new
+  # night_writer.read_and_write
 
