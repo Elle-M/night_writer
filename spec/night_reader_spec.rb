c5c20de407d1920db2eda45f1f62a74e3985e574
Elle Majors <hellolovely@ellemajors.com>
2023-01-17 16:40:55 -0700

    test: #braile_to_text can rejoin text after previous #text_to_braille line break

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -11,9 +11,6 @@ RSpec.describe NightReader do
   end
 
   it 'has attributes' do
-    #combine with attr_reader
-    # allow(night_writer).to recieve(:read_file).and_return('./message.txt')
-    # allow(night_writer).to recieve(:read_file).and_return('./braille.txt')
     night_reader.read_and_write
 
     expect(night_reader.read_file).to eq('./braille.txt')
@@ -21,15 +18,27 @@ RSpec.describe NightReader do
   end
 
   it 'has a hash that has braille keys and letter values' do
-    expect(night_reader.braille_to_letter.first).to eq([["0.", "..", ".."], "a"])
+    expect(night_reader.braille_to_letter.first).to eq([['0.', '..', '..'], 'a'])
   end 
 
-  it "#braile_to_text" do
-    expect(night_reader.braille_to_text("0.\n00\n..")).to eq('h')
+  it '#braile_to_text can write a letter to braile' do
+    expect(night_reader.braille_to_text('0.\n00\n..')).to eq('h')
   end 
 
-  it "#braile_to_text can take a word" do
-    word = "0.0.0.0.0.\n00.00.0..0\n....0.0.0."
+  it '#braile_to_text write word to braile' do
+    word = '0.0.0.0.0.\n00.00.0..0\n....0.0.0.'
     expect(night_reader.braille_to_text(word)).to eq('hello')
   end  
+
+  it '#braile_to_text does rejoin from above 80 characters' do
+    eighty_two_char_count = '0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.
+................................................................................
+................................................................................
+0.
+..
+..'
+    expected = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    #format expected better?
+    expect(braille_to_text(eighty_two_char_count)).to eq(expected)
+  end
 end
