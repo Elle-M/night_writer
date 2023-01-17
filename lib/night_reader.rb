4a1adb41ed62125873dfe347e6d612a741914815
Elle Majors <hellolovely@ellemajors.com>
2023-01-17 11:23:10 -0700

    refact: delete commented out unused code

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,61 +1,23 @@
 require_relative './alphabetable'
 class NightReader
+
   attr_accessor :read_file,
                 :write_file
-  # attr_reader :alphabet
   include Alphabetable
-
   def initialize
     @read_file = ARGV[0]
     @write_file = ARGV[1]
-    # @alphabet = {
-    #   "a" => ["0.", "..", ".."],
-    #   "b" => ["00", "..", ".."],
-    #   "c" => ["0.", ".0", ".."],
-    #   "d" => ["0.", ".0", "0."],
-    #   "e" => ["0.", "..", "0."],
-    #   "f" => ["00", ".0", ".."],
-    #   "g" => ["00", ".0", "0."],
-    #   "h" => ["00", "..", "0."],
-    #   "i" => [".0", ".0", ".."],
-    #   "j" => [".0", ".0", "0."],
-    #   "k" => ["0.", "0.", ".."],
-    #   "l" => ["00", "0.", ".."],
-    #   "m" => ["0.", "00", ".."],
-    #   "n" => ["0.", "00", "0."],
-    #   "o" => ["0.", "0.", "0."],
-    #   "p" => ["00", "00", ".."],
-    #   "q" => ["00", "00", "0."],
-    #   "r" => ["00", "0.", "0."],
-    #   "s" => [".0", "00", ".."],
-    #   "t" => [".0", "00", "0."],
-    #   "u" => ["0.", "0.", ".0"],
-    #   "v" => ["00", "0.", ".0"],
-    #   "w" => [".0", ".0", "00"],
-    #   "x" => ["0.", "00", ".0"],
-    #   "y" => ["0.", "00", "00"],
-    #   "z" => ["0.", "0.", "00"]
-    #   }
-    end
-
+  end
 
   def read_and_write
     braille_message = File.read(@read_file)
-    # require 'pry'; binding.pry
+    translated_text = braille_to_text(braille_message)
+    File.write(@write_file, translated_text)
     message = File.read(@write_file)
     char_count = message.chars.count
-    #refactor to count text
     puts "Created #{@write_file} contains #{char_count} characters"
-    
-    translated_text = braille_to_text(braille_message)
-    
-    File.write(@write_file, translated_text)
   end
 
-  # def braille_to_letter
-  #   alphabet.invert
-  # end
-# require 'pry'; binding.pry
   def braille_to_text(braille_message)
     braille_message_array = braille_message.split("\n")
     braille_arrays = braille_message_array.map do |braille_array|
@@ -68,20 +30,12 @@ class NightReader
         string_join.join
       end
     end
-    # require 'pry'; binding.pry
-    # braille_character = new_braille_message_array.each_slice(3).map do |braille_char|
-    #   braille_char if braille_char == braille_to_letter.keys_any?(braille_char)
-  #  tanspose
-      # braille_char 
-  end
-    
-#returns ["00", "0.", "00"]
-   
-    # require 'pry'; binding.pry
-    # braille_character.map do |braille|
-    #     braille if braille == braille_to_letter.keys 
-      # end
+    braille_to_letter_key = new_braille_message_array.transpose
+    translation_braille_to_text = braille_to_letter_key.filter_map do |letter|
+      braille_to_letter[letter]
+    end.join
+  end 
 end
-
+#comment out runner to run rspec
 # night_reader = NightReader.new
 # night_reader.read_and_write
