ff6682b3e6684e420a72d7ae569e35e9202cd55c
Elle Majors <hellolovely@ellemajors.com>
2023-01-16 17:12:37 -0700

    test: #braille_to_test

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe NightReader do
   let(:night_reader) { NightReader.new }
   before do
     night_reader.read_file =  './braille.txt'
-    night_reader.write_file = './message.txt'
+    night_reader.write_file = './original_message.txt'
   end
   it 'exists' do
     expect(night_reader).to be_a(NightReader)
   end
 
-  xit 'has attributes' do
+  it 'has attributes' do
     #combine with attr_reader
     # allow(night_writer).to recieve(:read_file).and_return('./message.txt')
     # allow(night_writer).to recieve(:read_file).and_return('./braille.txt')
@@ -20,7 +20,11 @@ RSpec.describe NightReader do
     expect(night_reader.write_file).to eq('./message.txt')
   end
 
-  it 'has a hash that has braille keys and letter values' do
+  xit 'has a hash that has braille keys and letter values' do
     expect(night_reader.braille_to_letter.first).to eq([["0.", "..", ".."], "a"])
   end 
+
+  it "#braile_to_text" do
+    expect(night_reader.braille_to_text('.00..0.0')).to eq("a")
+  end
 end
